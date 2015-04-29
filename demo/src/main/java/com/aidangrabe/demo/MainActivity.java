a8cf163f7925872e94a81289861dd0fb393eaa07
Aidan Grabe <aidsgrabe@gmail.com>
2015-04-29 20:09:16 +0100

    Restructure library by moving demo and library into separate modules

---
+++ demo/src/main/java/com/aidangrabe/demo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.aidangrabe.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.aidangrabe.materialcolorpicker.ColorPickerDialogFragment;
+
+
+public class MainActivity  extends ActionBarActivity implements ColorPickerDialogFragment.ColorSelectedListener {
+
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mButton = new Button(this);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ColorPickerDialogFragment dialog = new ColorPickerDialogFragment();
+                dialog.setOnColorSelectedListener(MainActivity.this);
+                dialog.show(getSupportFragmentManager(), "dialog");
+            }
+        });
+
+        setContentView(mButton);
+
+    }
+
+    @Override
+    public void onColorSelected(int color) {
+        mButton.setBackgroundColor(color);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
